4d2a281f71f14920e874f88e7cc285c9b1e8ecc7
x2boarder <x2boarder@gmail.com>
2017-07-09 19:29:02 -0400

    Changing endpoint location

--- express.js
+++ express.js
@@ -25,51 +25,62 @@ expressApp.post('/google-hvt-api', function (request, response) {
       make = app.getArgument('Make'),
       model = app.getArgument('Model');
 
-      console.log(`${year} ${make} ${model}`);
-
-    var factPrefix = "Didn't work";
-    let fact = "Value your vehicle!";
+    console.log(`${year} ${make} ${model}`);
 
     var strUrl = "https://eservices.hagerty.com/Api/Vehicle/v3/e72c154d/US/Vehicles/1/1965/122/3023/397/186/51";
     var request = fetch(strUrl)
-        .then(function(res) {
-            return res.text();
-        }).then(function(data) {
-            console.log(data);
-            console.log(app.getArgument('Year'));
-        });
+      .then(function (res) {
+        return res.text();
+      }).then(function (data) {
+        console.log(data);
+        console.log(app.getArgument('Year'));
 
-    console.log(request);
 
-/*
+        var factPrefix = "It worked";
+        let fact = "Value your vehicle!";
+        if (app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)) {
+          app.ask(app.buildRichResponse()
+            .addSimpleResponse(factPrefix)
+            .addBasicCard(app.buildBasicCard(fact)
+              .setImage('https://o.aolcdn.com/images/dims3/GLOB/crop/4220x2374+0+0/resize/800x450!/format/jpg/quality/85/http://o.aolcdn.com/hss/storage/midas/1cce1f0e74ac5eef38a9584739e02479/205232832/2018+Mustang+design+sketch++%283%29.jpg', 'Darth Vader Mustang'))
+            .addSimpleResponse('Would you like to value another vehicle Dan?')
+            .addSuggestions(['Sure', 'No thanks']));
+        } else {
+          app.ask('I have no idea what you just said');
+        }
 
-    var factPrefix = test[0].weightedAverageValue;
-    if (app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)) {
-      app.ask(app.buildRichResponse()
-        .addSimpleResponse(factPrefix)
-        .addBasicCard(app.buildBasicCard(fact)
-          .setImage('https://o.aolcdn.com/images/dims3/GLOB/crop/4220x2374+0+0/resize/800x450!/format/jpg/quality/85/http://o.aolcdn.com/hss/storage/midas/1cce1f0e74ac5eef38a9584739e02479/205232832/2018+Mustang+design+sketch++%283%29.jpg', 'Darth Vader Mustang'))
-        .addSimpleResponse('Would you like to value another vehicle Dan?')
-        .addSuggestions(['Sure', 'No thanks']));
-    } else {
-      app.ask('I have no idea what you just said');
-    }
+      });
 
+    
     /*
 
-    console.log(app.getArgument('Year'));
+        var factPrefix = test[0].weightedAverageValue;
+        if (app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)) {
+          app.ask(app.buildRichResponse()
+            .addSimpleResponse(factPrefix)
+            .addBasicCard(app.buildBasicCard(fact)
+              .setImage('https://o.aolcdn.com/images/dims3/GLOB/crop/4220x2374+0+0/resize/800x450!/format/jpg/quality/85/http://o.aolcdn.com/hss/storage/midas/1cce1f0e74ac5eef38a9584739e02479/205232832/2018+Mustang+design+sketch++%283%29.jpg', 'Darth Vader Mustang'))
+            .addSimpleResponse('Would you like to value another vehicle Dan?')
+            .addSuggestions(['Sure', 'No thanks']));
+        } else {
+          app.ask('I have no idea what you just said');
+        }
 
-    let rawData = app.getRawInput();
-    let matchVehicle = rawData.match(/\d{4}.*$/ig);
-    let factPrefix;
-    let fact = "Value your vehicle!";
+        /*
 
-    if (app.getRawInput().indexOf('ford mustang') > 1) {
-      factPrefix = "Why yes, your" + matchVehicle + " is worth $57,000. Impressive...most impressive.";
-    } else {
-      factPrefix = 'Sorry, we couldn\'t find a value for that vehicle. Please try again.';
-    }
-     */
+        console.log(app.getArgument('Year'));
+
+        let rawData = app.getRawInput();
+        let matchVehicle = rawData.match(/\d{4}.*$/ig);
+        let factPrefix;
+        let fact = "Value your vehicle!";
+
+        if (app.getRawInput().indexOf('ford mustang') > 1) {
+          factPrefix = "Why yes, your" + matchVehicle + " is worth $57,000. Impressive...most impressive.";
+        } else {
+          factPrefix = 'Sorry, we couldn\'t find a value for that vehicle. Please try again.';
+        }
+         */
 
   }
 
