0ca5023fb028eb0948198d06a9e1adeb0144a407
x2boarder <x2boarder@gmail.com>
2017-07-16 15:04:38 -0400

    Very simple card response

--- helpers/numbers-with-commas.js
+++ helpers/numbers-with-commas.js
@@ -1,4 +1,4 @@
-module.export = function(x){
+module.exports = function(x){
     var parts = x.toString().split(".");
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     return parts.join(".");
