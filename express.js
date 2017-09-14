2bcd0f23be911bd27a6b1631f9e5b69468673853
x2boarder <x2boarder@gmail.com>
2017-09-14 08:20:45 -0400

    Building basic cards

--- express.js
+++ express.js
@@ -15,97 +15,97 @@ expressApp.get('/', function(request, response) {
 
 // answer request
 expressApp.post('/google-hvt-api', function(request, response) {
-            var GoogleApp = new google({
-                request,
-                response
+    var GoogleApp = new google({
+        request,
+        response
+    });
+
+    function tellValue(app) {
+
+        // structure API query
+        var year = app.getArgument('Year'),
+            make = app.getArgument('Make'),
+            model = app.getArgument('Model');
+
+        console.log(`${year} ${make} ${model}`);
+
+        // shape decode strings
+        var vehicleString = {
+            year: app.getArgument('Year'),
+            make: app.getArgument('Make'),
+            model: app.getArgument('Model')
+        };
+        var vehicleToDecode = JSON.stringify(vehicleString);
+
+        // first decode URL
+        var decodeVehicle = 'https://www.hagerty.com/apps/valuationtools/Api/Search/Decode/Vehicle';
+        var vehicleInfoURL = "https://eservices.hagerty.com/Api/Vehicle/v3/e72c154d/US/Vehicles/1/";
+
+        // make first call
+        var result = fetch(decodeVehicle, {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                },
+                body: vehicleToDecode
+            }).then(function(response) {
+                return response.json();
+            }).then(function(vehicleData) {
+
+                // take decode data and make request to api
+                var year = vehicleData.year.id,
+                    make = vehicleData.make.id,
+                    model = vehicleData.model.id;
+
+                var valueRequestURLBuilder = `${vehicleInfoURL}${year}/${make}/${model}`;
+
+                // make second call
+                return fetch(valueRequestURLBuilder, {
+                    method: 'get'
+                });
+
+            })
+            .then(function(response) {
+                return response.json();
+            }).then(function(vehicleValue) {
+                //display 2nd call
+                console.log(vehicleValue);
+
+                // first value
+                var firstCarFullName = vehicleValue[0].text,
+                    firstCarAverageValue = numberWithCommas(vehicleValue[0].weightedAverageValue);
+
+                var carValue = `The ${firstCarFullName} is worth $${firstCarAverageValue}`;
+                if (app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)) {
+                    app.ask(app.buildRichResponse()
+                        .addSimpleResponse(carValue)
+                        .addBasicCard(app.buildBasicCard(carValue)
+                            .setImage('https://www.hagerty.com/content/img/logo.svg', 'Hagerty'))
+                        .addSimpleResponse('Would you like to value another vehicle?')
+                        .addSuggestions(['Sure', 'No thanks']));
+                } else {
+                    app.ask('I have no idea what you just said');
+                }
+
+                function numberWithCommas(x) {
+                    var parts = x.toString().split(".");
+                    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                    return parts.join(".");
+                }
+
+            })
+            .catch(function(error) {
+                console.log('Request failed', error)
             });
 
-            function tellValue(app) {
+    }
 
-                // structure API query
-                var year = app.getArgument('Year'),
-                    make = app.getArgument('Make'),
-                    model = app.getArgument('Model');
+    let actionMap = new Map();
+    actionMap.set('tell.value', tellValue);
+    GoogleApp.handleRequest(actionMap);
+});
 
-                console.log(`${year} ${make} ${model}`);
-
-                // shape decode strings
-                var vehicleString = {
-                    year: app.getArgument('Year'),
-                    make: app.getArgument('Make'),
-                    model: app.getArgument('Model')
-                };
-                var vehicleToDecode = JSON.stringify(vehicleString);
-
-                // first decode URL
-                var decodeVehicle = 'https://www.hagerty.com/apps/valuationtools/Api/Search/Decode/Vehicle';
-                var vehicleInfoURL = "https://eservices.hagerty.com/Api/Vehicle/v3/e72c154d/US/Vehicles/1/";
-
-                // make first call
-                var result = fetch(decodeVehicle, {
-                        method: 'post',
-                        headers: {
-                            'Accept': 'application/json, text/plain, */*',
-                            'Content-Type': 'application/json'
-                        },
-                        body: vehicleToDecode
-                    }).then(function(response) {
-                        return response.json();
-                    }).then(function(vehicleData) {
-
-                        // take decode data and make request to api
-                        var year = vehicleData.year.id,
-                            make = vehicleData.make.id,
-                            model = vehicleData.model.id;
-
-                        var valueRequestURLBuilder = `${vehicleInfoURL}${year}/${make}/${model}`;
-
-                        // make second call
-                        return fetch(valueRequestURLBuilder, {
-                            method: 'get'
-                        });
-
-                    })
-                    .then(function(response) {
-                        return response.json();
-                    }).then(function(vehicleValue) {
-                            //display 2nd call
-                            console.log(vehicleValue);
-
-                            // first value
-                            var firstCarFullName = vehicleValue[0].text,
-                                firstCarAverageValue = numberWithCommas(vehicleValue[0].weightedAverageValue);
-
-                            var carValue = `The ${firstCarFullName} is worth $${firstCarAverageValue} dollars`;
-                            if (app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT)) {
-                                app.ask(app.buildRichResponse()
-                                    .addSimpleResponse(carValue)
-                                    .addBasicCard(app.buildBasicCard(carValue)
-                                        .addSimpleResponse('Would you like to value another vehicle?')
-                                        .addSuggestions(['Sure', 'No thanks']));
-                                }
-                                else {
-                                    app.ask('I have no idea what you just said');
-                                }
-
-                                function numberWithCommas(x) {
-                                    var parts = x.toString().split(".");
-                                    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                                    return parts.join(".");
-                                }
-
-                            })
-                        .catch(function(error) {
-                            console.log('Request failed', error)
-                        });
-
-                    }
-
-                let actionMap = new Map();
-                actionMap.set('tell.value', tellValue);
-                GoogleApp.handleRequest(actionMap);
-            });
-
-        var server = expressApp.listen(expressApp.get('port'), function() {
-            console.log('Node app is running on port', expressApp.get('port'));
-        });
+var server = expressApp.listen(expressApp.get('port'), function() {
+    console.log('Node app is running on port', expressApp.get('port'));
+});
